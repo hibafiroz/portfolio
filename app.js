9c18eada56cd5dc33109108b6feca478063f0cbc
Hiba <hiba.dev@gmail.com>
2025-10-20 13:36:27 +0530

    portfoli

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const dotenv=require('dotenv').config()
 const userRouter = require('./Routes/user-route')
 const adminRouter = require('./Routes/admin-route')
-const PORT = 5505
+const PORT = process.env.PORT
 
 app.use(express.json())
 app.use(express.static(path.join('public')))
